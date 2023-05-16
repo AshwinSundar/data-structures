8ac4abc5b9dd381b5f000af36f093c07b85b9021
Ashwin Sundar <ashwin.sundar@devetry.com>
2023-05-15 22:29:03 -0600

    clean up

--- cpp/node.cpp
+++ cpp/node.cpp
@@ -34,31 +34,6 @@ int main() {
 	std::cout << "EY" << std::endl;
 	return 0;
 };
-/*
-class Node<int> {
-	public:
-		Node() = delete;
-		Node(int val);
-		Node(int val, Node parentNode, Node leftNode, Node rightNode);
-		int _balance;
-		int _value; // change to const
-		void addChild(Node& node);	
-		void find(int val);
-	private:
-		// std::optional<Node> _parentNode;
-		// Node& _leftNode;
-		// Node& _rightNode;
-};
-*/
-
-// is this needed? let's implement it anyway, as an exercise
-// implement as singleton, in separate file. import here
-/*
-class NullNode {
-	public:
-		NullNode() = delete;
-};
-*/
 
 // SMART POINTERS
 // The use case for a smart pointer became obvious to me when I was attempting to solve a particular problem - what happens if a Node's parent or children are deleted? The obvious solution was to define a destructor that accessed the Node's parent and children, and removed corresponding link from their side. But then this required access to other Node's private members. The solution is to use a smart pointer. According to to cppreference.com, **Smart pointers are used to make sure that an object is deleted if it is no longer used (referenced)**. 
