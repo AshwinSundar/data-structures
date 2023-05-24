7a7d3ec012562fce7d5c15cd687dc73924297aae
Ashwin Sundar <ashwin.sundar@devetry.com>
2023-05-23 22:05:04 -0600

    update node

--- cpp/node.cpp
+++ cpp/node.cpp
@@ -1,40 +1,104 @@
-#include <optional>
 #include <iostream>
 #include <memory>
+#include <optional>
 
 template<typename T>
 class Node
 {
 	public:
-		// constructors
-		Node<T>(int val) : _value(val), _parentNode(nullptr), _leftNode(nullptr), _rightNode(nullptr) { };
-		Node<T>(int val, Node<T>& parent, Node<T>& left, Node<T>& right) : _value(val), _parentNode(parent), _leftNode(left), _rightNode(right) { };
-		// ~Node<T> // do something on destruction. send a note somewhere? maybe parent and child nodes need to know about it actually
-		// SMART POINTERS are the solution! Instead of doing manual cleanup with a destructor, let a smart pointer handle it automatically
+		Node<T>(int val) : _value(val), _balance(0) { };
+
+		Node<T>(int val, Node<T>& left, Node<T>& right)
+			: _value(val), _leftNode(left), _rightNode(right)
+		{
+			// updateBalance();	// TO DO
+		}
+
+		~Node<T>() 
+		{
+			// check if child pointers, parents are connected to anything, delete if not
+		}
+	
+		friend std::ostream & operator << (std::ostream &out, const Node<T> &n) {
+			out << n._value;	
+			if (n._leftNode) { out << "--" << n._leftNode; }
+			if (n._rightNode) { out << "\n|\n" << n._rightNode; }
+			return out << std::endl;	
+		}
+
 		T getValue() { return _value; }
+
 		T getBalance() { return _balance; }
-		std::unique_ptr<Node<T>> _parentNode;
-		std::unique_ptr<Node<T>> _leftNode;
-		std::unique_ptr<Node<T>> _rightNode;
+
+		void addChild(Node<T>& n)
+		{
+			if (n.getValue() < _value) { !_leftNode ?  _leftNode = n : _leftNode->addChild(n); }
+			else { !_rightNode ? _rightNode = n : _rightNode->addChild(n); }
+		}
+
+		Node<T>& _parentNode;
+		Node<T>& _leftNode;
+		Node<T>& _rightNode;
 
 	private:
+		void setBalance(int bal) { _balance = bal; }
+
+		void updateBalance() { } // TO DO
+
+		Node<T>& find(T val) { } // TO DO
+
+		// opportunity for memoization?
+		bool contains(T val) { } // TO DO 
+
 		T _value;
 		int _balance;
-		void setBalance(int bal) { _balance = bal; }
-		void calcBalance(); 
-		void addChild(Node<T>); // re-calc balance at end
-		std::unique_ptr<Node<T>> find(T val);
-		bool contains(T val); // opportunity for memoization?
-		// void print(); 
 };
 
 int main() {
-	Node<int> int_node(1);
-	// std::cout << "New node has value: " << (std::string)int_node.getValue() << std::endl;
-	std::cout << "EY" << std::endl;
+	Node<int> nodeA(2);
+	Node<int> nodeB(3);
+	Node<int> headNode(1, nodeA, nodeB);
+	// Node<int> headNode(1, std::make_unique<Node<int>>(nodeA), std::make_unique<Node<int>>(nodeB)); // problems here
+	// See ~4:30 -> https://www.youtube.com/watch?v=UOB7-B2MfwA
+
+	// headNode.addChild(childNode); // some issues here
+	// std::cout << headNode << std::endl;
 	return 0;
 };
 
 // SMART POINTERS
 // The use case for a smart pointer became obvious to me when I was attempting to solve a particular problem - what happens if a Node's parent or children are deleted? The obvious solution was to define a destructor that accessed the Node's parent and children, and removed corresponding link from their side. But then this required access to other Node's private members. The solution is to use a smart pointer. According to to cppreference.com, **Smart pointers are used to make sure that an object is deleted if it is no longer used (referenced)**. 
-// test whether you want unique_ptr and shared_ptr. What happens to when an object is deleted? Does the pointer keep existing? Try inspecting with gdb. Could be a cool article to test it out as well. You will learn a LOT about pointers if you can get through that. 
+// test whether you want unique_ptr and shared_ptr. What happens to when an object is deleted? Does the pointer keep existing? Try inspecting with gdb. Could be a cool article to test it out as well. You will learn a LOT about pointers if you can get through that. Use gdb
+
+// PATTERN MATCHING
+// Without pattern matching, the addChild method looks like this:
+/*
+		void addChild(Node<T> n)
+		{
+			std::unique_ptr<Node<T>> childNode = std::make_unique<Node<T>>(n.getValue());
+			if (n.getValue() < _value) 
+			{
+				if (!_leftNode)
+				{
+					_leftNode = n;
+				}	
+				else 
+				{
+					_leftNode.addChild(n);
+				}
+			}
+			else
+			{
+				if (!_rightNode)
+				{
+					_rightNode = n;
+				}	
+				else
+				{
+					_rightNode.addChild(n);
+				}
+			}
+		}
+*/
+
+// Not very pretty. 
